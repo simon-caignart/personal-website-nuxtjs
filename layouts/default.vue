9c45df6ff29ea57bb9a167e800bf803aebcb84ce
Simon Caignart <simon.caignart@gmail.com>
2021-08-29 22:04:40 +0200

    Fixed dark mode support

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,18 +14,58 @@ export default {
         this.$store.dispatch("autoTheme");
       });
   },
+  mounted: function() {
+    //this.switchLocale(this.getUserSupportedLocale());
+  },
+  methods: {
+/*     switchLocale(locale) {
+      if (this.$i18n.locale !== locale) {
+        this.$i18n.locale = locale;
+        this.setI18nLocaleInServices(locale);
+      }
+    },
+    getUserSupportedLocale() {
+      const userPreferredLocale = this.getUserLocale();
+
+      if (this.isLocaleSupported(userPreferredLocale.locale)) {
+        return userPreferredLocale.locale;
+      }
+
+      if (this.isLocaleSupported(userPreferredLocale.localeNoISO)) {
+        return userPreferredLocale.localeNoISO;
+      }
+      return this.defaultlocale;
+    },
+    isLocaleSupported(locale) {
+      return this.supportedLocales.includes(locale);
+    },
+    getUserLocale() {
+      const locale =
+        window.navigator.language ||
+        window.navigator.userLanguage ||
+        this.defaultLocale;
+      return {
+        locale: locale,
+        localeNoISO: locale.split("-")[0],
+      };
+    },
+    setI18nLocaleInServices(locale) {
+      this.currentLocale = locale;
+      axios.defaults.headers.common["Accept-Language"] = locale;
+      document.querySelector("html").setAttribute("lang", locale);
+      return locale;
+    }, */
+  },
   computed: {
     ...mapGetters({ theme: "getTheme" })
   },
   watch: {
     theme(newTheme, oldTheme) {
       if (newTheme === "light") {
-        console.log("lumière 💡");
         document.querySelector("html").classList.remove("dark");
         document.querySelector("html").classList.remove("bg-gray-800");
         document.querySelector("html").classList.add("bg-white");
       } else {
-        console.log("nuit 🌑");
         document.querySelector("html").classList.add("dark");
         document.querySelector("html").classList.remove("bg-white");
         document.querySelector("html").classList.add("bg-gray-800");
